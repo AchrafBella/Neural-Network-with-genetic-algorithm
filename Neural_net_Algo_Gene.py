0d84c1e9df46c1850661aabd3a53927a342f8ea5
SuperNova <52492864+AchrafBella@users.noreply.github.com>
2020-05-27 08:12:27 +0200

    Update Neural_net_Algo_Gene.py

--- Neural_net_Algo_Gene.py
+++ Neural_net_Algo_Gene.py
@@ -1,108 +1,123 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 25 16:34:05 2020
+Created on Tue May 26 23:27:43 2020
 
 @author: Supernova
 """
 import numpy as np
-np.random.seed(1)
+from scipy.special import expit
+
 # loss fonction to calculate the error 
-def rmse(predictions, targets):
+# Mean Squared Error (MSE)
+def mse(predictions, targets):
     differences = predictions - targets
     differences_squared = differences ** 2
     mean_of_differences_squared = differences_squared.mean()
     rmse_val = np.sqrt(mean_of_differences_squared)
     return rmse_val
 
-# fonction activation : sigmoid
-def sigmoid_derive(x, derive = False):
-    if( derive == True):
-        return x*(1-x)
-    else:
-        return 1/(1+ np.exp(-x))
+class Neural_Network():
+    def __init__(self, input_nodes, output_nodes, size):
+        #les parametres de notre réseau 
+        self.input_nodes  = input_nodes
+        self.output_nodes = output_nodes
+        self.size = size
+        
+        self.weights = list()   
+        # fonction activation : sigmoid
+        self.sigmoid = lambda x : expit(x)
+        pass
+        
+    def __Population(self):
+        self.weights = [(2*np.random.random((self.input_nodes, self.output_nodes))-1).tolist() for _ in range(self.size)]
+        return self.weights
+
+    def __Fitness(self, x, y):
+        liste = list()
+        error = list()
+        for elm in self.weights:
+            output = self.sigmoid(np.dot(x,elm))
+            liste.append(output)
+            elm += [mse(output, y)]
+            error.append(mse(output, y))
+            pass
+        return liste, np.mean(error)
+    #fonction de la selection des 10 premiers
+    #pour determiner les synapes qui sont bonne il faut calculer l'accuracy à
+    # travers RMSE nous avons ajouter l'accuracy dans la popualtion des synapes
+    # comme le tri la fonction sort( key = lambda accuracy_liste : 
+    # accuracy_liste[-1], reverse = True) pour le tri des premiers element selon 
+    #l'accuracy comme on a fait le tri nous ne sommes plus besoin de l'accuracy on
+    #le supprimer par la fonction del et en retour la top des synapes
+    def __Selection(self):
+        self.weights = sorted(self.weights, key = lambda liste : liste[-1])
+        for elm in self.weights:
+            del elm[-1]
+            pass
+        pass
+    #fonction de croissement
+    #le choix des parent aletoire
+    #la creation des enfant (les nouveaux synapes)
+
+    def __Crossover(self, top):
+        top_weight = self.weights[:top]
+        parent1, parent2 = top_weight[np.random.randint(top)],  top_weight[np.random.randint(top)]
+        while(parent1 == parent2):
+            parent2 = top_weight[np.random.randint(top)]
+            pass
+        neural = (np.mean([parent1, parent2], axis = 0)).tolist()
+        del self.weights[-1]
+        self.weights.append(neural)
+        pass
     
-# données d'entrainement 
-x_train = np.array([[0,1,1],[0,1,1],[1,0,1],[1,1,1]])
-y_train = np.array([[0,0,1,1]]).T
+    def __Mutation(self):
+        for elm in self.weights:
+            split = np.random.randint(3)
+            elm[split][0] += (np.random.uniform(-1.0, 1.0, 1))[0]            
+            pass
+        pass
+    
+    def fit(self, x, y, epochs, top = 5):
+        """
+        fonction fit pour entrain notre réseau de neurones 
+        Parameters
+        ----------
+        x : TYPE
+            DESCRIPTION.
+        y : TYPE
+            DESCRIPTION.
+        epochs : TYPE
+            DESCRIPTION.
+        top : TYPE, optional
+            DESCRIPTION. The default is 5.
 
-# creation de la population des synapses 
-def population(size_population):
-    population = [(2*np.random.random((3,1))-1).tolist() for _ in range(size_population)]
-    return population
+        Returns
+        -------
+        output : TYPE
+            DESCRIPTION.
 
-# fonction fitness
-# param : pop == popualtion
-# la fontion tolist() pour transformer une array a une liste
-def fitness(pop, Input):
-    output = []
-    for elm in pop:
-        output.append( np.abs((sigmoid_derive(np.dot(Input, elm)).tolist() )))
-    return output
-
-#fonction de la selection des 10 premiers
-#pour determiner les synapes qui sont bonne il faut calculer l'accuracy à travers RMSE
-#nous avons ajouter l'accuracy dans la popualtion des synapes comme le tri 
-#la fonction sort( key = lambda accuracy_liste : accuracy_liste[-1], reverse = True) pour le tri des premiers element selon l'accuracy
-# comme on a fait le tri nous ne sommes plus besoin de l'accuracy on le supprimer par la fonction del et en retour la top des synapes
-def selection(pop, output, real_output):
-    i = 0
-    for elm in output:
-        pop[i] += [rmse(elm, real_output)]
-        i += 1
-    pop.sort( key = lambda accuracy_liste : accuracy_liste[-1], reverse = True)
-    for elm in pop:
-        del elm[-1]
-    #print("Vamos com muita calma nessa hora, Selection!: pop,", pop, sep="\n")
-    return pop
-
-#fonction de croissement
-#le choix des parent aletoire
-#la creation des enfant (les nouveaux synapes)
-
-def crossover(pop, top):
-    #les top premiers selon l'accuracy
-    top_10 = pop[:top]
-    # le split ou le crossover point
-    split = np.random.randint(4)
-    parent1 = top_10[np.random.randint(top)]
-    parent2 = top_10[np.random.randint(top)]
-    child1 = parent1[0:split] + parent2[split:4]
-    child2 = parent2[0:split] + parent1[split:4]
-    # j'ai supprimer les 10 premiers dans la liste qui contient les 20 synapes (la population)
-    del pop[:top]
-    # j'ai sypprimer les parent et j'ai ajouter les nouveaux enfant
-    if( parent1 == parent2):
-        top_10.remove(parent1)
-    else:
-        top_10.remove(parent1)
-        top_10.remove(parent2)
-    top_10.append(child1)
-    top_10.append(child2)
-    # puis j'ai ajouter la nouvelle liste de top 10 dans la liste de la population
-    pop.extend(top_10)
-    return pop
-
-#mutation
-def mutation(pop):
-    for elm in pop:
-        split = np.random.randint(3)
-        elm[split][0] += (np.random.uniform(-1.0, 1.0, 1))[0]
-    return pop
-
+        """
+        self.__Population()
+        for e in range(epochs):
+            output, error = self.__Fitness(x, y)
+            self.__Selection()
+            self.__Crossover(top)
+            self.__Mutation()
+            if(e % 5 == 0):
+                print('epoch ==>', e, 'loss ==>',error)
+            pass
+        return output
+pass
+    
 if __name__ == '__main__':
-    generation = 4
-    m = None
-    if(m == None):
-        p = population(50)
-    else:
-        p=m
-    for i in range(generation):
-        f = fitness(p, x_train)
-        s = selection(p, f, y_train)
-        print("s : ",*s, sep="\n")
-        print("\n ")
-        c = crossover(s, 10)
-        m = mutation(c)
-        print('generation : ',i)
-    for elm in f:
-        print(elm,'\n') 
+    # données d'entrainement 
+    x = np.array([ [0,0,1],
+                          [1,1,1],
+                          [1,0,1],
+                          [0,1,1]])
+    
+    y = np.array([[0,1,1,0]]).T
+    
+    n = Neural_Network(3, 1, 500)
+    t = n.fit(x, y,150, 100)
+    print(*t[0:5], sep = '\n')
